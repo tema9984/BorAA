b335be980432dff89fb70609601bb2f8e17fed94
tema9984 <47312875+tema9984@users.noreply.github.com>
2019-03-07 16:56:43 +0300

    Update dz2.sql

--- dz2.sql
+++ dz2.sql
@@ -1,62 +1,63 @@
-1.__________________________________________________________________________________________________________________
 SELECT n_group, COUNT(n_group)
 FROM student21
 GROUP BY n_group
 
-2.__________________________________________________________________________________________________________________
+2.
 SELECT MAX(score), n_group
 FROM student21
 GROUP BY n_group
 
-3.__________________________________________________________________________________________________________________
+3.
 SELECT count(surname),surname
 FROM student21
 GROUP BY surname
 
-4.__________________________________________________________________________________________________________________
+4.
 SELECT Extract(YEAR from date_birth),count(date_birth)
 FROM student21
 GROUP BY Extract(YEAR from date_birth) 
 
-5.__________________________________________________________________________________________________________________
-SELECT trunc(n_group,3), AVG(SCORE)
+5.
+SELECT n_group, AVG(SCORE)
 FROM student21
 GROUP BY n_group
 
-6.__________________________________________________________________________________________________________________
+6.
 SELECT max(score), n_group
 FROM student21
 where n_group like '2%'
 GROUP BY n_group
 
-7.__________________________________________________________________________________________________________________
-SELECT AVG(score) as avgscore, n_group
+7.
+SELECT AVG(score) as score, n_group
 FROM student21
 GROUP BY n_group
 having avg(score)<=3.5
-order by avgscore asc
+order by score asc
 
-8.__________________________________________________________________________________________________________________
+8.
 Select * 
 FROM (
-      SELECT max(risk), name
-      FROM hobby
-      GROUP BY name 
-	 )t
-WHERE rownum<=3
+SELECT max(risk), name
+FROM hobbies
+group by name
+order by max(risk) desc
+ )
+WHERE rownum<=3 
 
-9.__________________________________________________________________________________________________________________
-SELECT n_group, count(n_z), max(score), avg(score), min(score)
+
+9.
+SELECT n_group, count(nz), max(score), avg(score), min(score)
 FROM student21
-GROUP BY n_group
+GROUP BY n_group 
 
-????????????10.__________________________________________________________________________________________________________________
-SELECT name, n_z, surname, score, n_group
+10.
+SELECT name, nz, surname, score, n_group
 FROM student21
-where n_group like '2253'
-ORDER BY score DESC FETCH FIRstudent21 1 ROWS ONLY
+where n_group = :n_group
+ORDER BY score DESC FETCH FIRST 1 ROWS ONLY
 
-11.__________________________________________________________________________________________________________________
+11.
 SELECT *
 FROM(
      SELECT n_group, max(score) as ms

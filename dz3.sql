b0f5fe7c24352e3422997d7f07b799c3ba842222
tema9984 <47312875+tema9984@users.noreply.github.com>
2019-04-04 17:26:36 +0300

    Update dz3.sql

--- dz3.sql
+++ dz3.sql
@@ -1,3 +1,4 @@
+1.
 SELECT student21.Name, 
 student21.Surname, 
 STU_HOB.HOB_ID,
@@ -6,3 +7,20 @@ FROM student21,
 STU_HOB,
 HOBBIES
 WHERE STU_HOB.HOB_ID=HOBBIES.ID and student21.Nz= STU_HOB.Nz; 
+2. 
+SELECT * 
+FROM 
+(SELECT student21.Name, 
+student21.Surname, 
+stu_hob.hob_id, 
+CASE 
+WHEN date_finish is null THEN sysdate-stu_hob.date_start 
+ELSE stu_hob.date_finish-stu_hob.date_start 
+END AS status
+from
+student21, 
+stu_hob 
+WHERE student21.NZ=stu_hob.NZ 
+GROUP BY date_finish, date_start, hob_id, name, surname 
+ORDER BY status desc) 
+WHERE rownum=1 
